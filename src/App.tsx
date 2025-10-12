2f2fba47fdefce93b969e5b50341e44c0c901383
SonOfMan12316 <mancycarter@gmail.com>
2025-10-12 21:01:31 +0100

    fully scaffold and tailwind works we strike at dawnnpx tailwindcss init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-400 p-4 border-b-4 border-teal-500">
+        Vite + React + TS + Tailwind Works!
+      </h1>
+    </div>
+  );
+}
+
+export default App
