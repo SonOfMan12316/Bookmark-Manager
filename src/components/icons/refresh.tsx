79bdbe21ccd884f62056e0695d503e18d3dd4732
SonOfMan12316 <mancycarter@gmail.com>
2025-11-09 23:59:19 +0100

    Added new icons

---
+++ src/components/icons/refresh.tsx
@@ -0,0 +1,22 @@
+const Refresh = ({ ...props }) => {
+  return (
+    <svg
+      width="15"
+      height="14"
+      viewBox="0 0 15 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M0.75 5.41667C0.75 5.41667 2.08665 3.59548 3.17256 2.50883C4.25846 1.42218 5.75907 0.75 7.41667 0.75C10.7304 0.75 13.4167 3.43629 13.4167 6.75C13.4167 10.0637 10.7304 12.75 7.41667 12.75C4.68127 12.75 2.37341 10.9195 1.65118 8.41667M0.75 5.41667V1.41667M0.75 5.41667H4.75"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default Refresh
