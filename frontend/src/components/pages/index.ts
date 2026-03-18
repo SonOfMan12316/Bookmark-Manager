bd5c50e0bb92252490e2f8b93453562cb5684e2e
SonOfMan12316 <mancycarter@gmail.com>
2026-03-18 11:00:03 +0100

    chore: remove password reset pages and disable mail service

--- frontend/src/components/pages/index.ts
+++ frontend/src/components/pages/index.ts
@@ -1,5 +1,3 @@
 export { default as SignIn } from './sign-in'
 export { default as SignUp } from './sign-up'
-export { default as ForgotPassword } from './forgot-password'
-export { default as ResetPassword } from './reset-password'
 export { default as Home } from './home'
